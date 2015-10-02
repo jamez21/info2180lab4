e0c45cd9b124041ab7406706a293cd3c5c8e3956
jamez21 <10thfloor@live.com>
2015-10-02 11:53:21 -0500

    first commit

---
+++ maze.js
@@ -0,0 +1,50 @@
+"use strict";
+var win = true;
+
+
+window.onload = function(){
+    $("end").onmouseover = gameEnd;
+    $("start").onclick = gameReset;
+    //$("start").onclick = antiCheat;
+    var walls = $$("div#maze div.boundary");
+    for (var x=0;x < walls.length;x++){
+        walls[x].onmouseover = mouseOver;
+    }
+};
+
+function mouseOver(){
+    win = false;
+    var walls = $$("div#maze div.boundary");
+    for(var x=0;x < walls.length;x++){
+        walls[x].addClassName("youlose");
+    }
+    gameEnd();
+}
+
+function gameEnd(){
+    if (win){
+        $("status").innerHTML=("CONGRADULATIONS. YOU WON!!!!");
+    }else{
+        $("status").innerHTML=("You Lose. Try Again");
+    }
+}
+
+function gameReset(){
+    win = true;
+    $("status").innerHTML=("Find The End");
+    var walls = $$("div#maze div.boundary");
+    for(var x=0;x < walls.length;x++){
+        walls[x].removeClassName("youlose");
+    }
+}
+
+/*
+function antiCheat(){
+    var walls = $("div#maze");
+    for(x in walls){
+        if (!(walls[x].mouseover)){
+            win = false;
+            mouseOver();
+        }
+    }
+}*/
